dedf4286e83980c75064ba19e9b1e68316266a67
M-Kasper-93 <megan.kasper@csuglobal.edu>
2025-04-22 18:20:06 +0000

    version 1 start

---
+++ RadixSort.java
@@ -0,0 +1,12 @@
+//establish array for integers
+public static Integer[] msd(Integer[] items) {
+    //return item at index = 0
+    return msd(items, index: 0); 
+}
+//recursive function 
+private static Integer[] msd(Integer[] items, int index) {
+    //base case: if array has only one item, then it cannot be sorted
+    if (array.length <= 1) {
+        return items;
+    }
+}
